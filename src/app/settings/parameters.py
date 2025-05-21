bf7ef6ffa0ee1f9f7b18dd7f97c5a27bc0646332
Eduardo Rosa <edu.wd2014@gmail.com>
2025-05-20 21:42:06 -0300

    feat: update main

--- src/app/settings/parameters.py
+++ src/app/settings/parameters.py
@@ -7,6 +7,12 @@ class Parametros(enum.Enum):
     company_id = "0d0c1a67-503f-4fa5-a2e4-40b945339f20"
 
     # TOKEN DE AUTENTICAÇÃO
-    url_api_golang_token = "http://localhost:8080/api/v1/login"
+    url_api_golang_token = "http://localhost:8080/api/v1/login/token"
     user_token_api = "user_token_api"
     password_toke_api = "senha123"
+
+    # TESTES
+    status_teste = 1
+    token_teste = "aaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
+    feature_toggle_object = True
+    feature_toggle_call_api = True
